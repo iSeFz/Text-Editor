7e905ed5d86d3eaf6ab22ddedfc21b9aeabaa371
SeifYahia <seifyahia@gmail.com>
2022-05-11 18:34:22 +0200

    Added new function declarations

--- functions.h
+++ functions.h
@@ -12,4 +12,16 @@ void getFile();
 // Displays the menu to the user to choose what to do with the file
 void start();
 
+// 1 ==> Add more text to the file as input through console
+void addText(char filename[]);
+
+// 2 ==> Display contents of the file in the console
+void displayContent(char filename[]);
+
+// 3 ==> Remove/Truncate contents of the file
+void emptyFile(char filename[]);
+
+// 4-5 ==> Encrypt Or Decrypt contents of the file
+void encryptORdecrypt(char filename[], int decision);
+
 #endif
