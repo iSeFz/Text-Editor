c130ae81c9701aee2c1845349023646ed5c06bb1
SeifYahia <seifyahia@gmail.com>
2022-05-11 16:02:03 +0200

    Added the functions header

---
+++ functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Here we will put function prototypes
+
+// The main file name to use across the whole program
+char fileName[101];
+
+// Takes the initial file from the user to work on
+void getFile();
+
+// Displays the menu to the user to choose what to do with the file
+void start();
+
+#endif
