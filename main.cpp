22ff70f2b3a08a8a24675aac3ff79eba2f0c648a
SeifYahia <seifyahia@gmail.com>
2022-05-20 19:14:28 +0200

    Updated version of the main file

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@ Author1 and ID: Seif Yahia - 20210172
 Author2 and ID: Mohamed Amgad - 20210330
 Author3 and ID: Omar Elgammal - 20210271
 Program Name: Text Editor
-Last Modification Date: 16 May 2022
-Version: 3.5
+Last Modification Date: 20 May 2022
+Version: 4.0
 */
 
 #include "txteditlib.cpp"
 
 int main(){
-    cout << "\t\t\tWelcome to the Text Editor program!\n";
+    cout << "\t\t\tWelcome to the Text Editor program!";
     getFile();      // Takes the file from the user
     start();        // Displays the menu of options
+    return 0;
 }
